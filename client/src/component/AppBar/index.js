2baf1ad48c881a68e55ded1cf0cef173187c7bf0
kunalZM <kunal.1134@zenmonk.tech>
2024-04-17 10:14:14 +0530

    Feature: fixed app bar and docker testing

--- client/src/component/AppBar/index.js
+++ client/src/component/AppBar/index.js
@@ -34,7 +34,7 @@ const AppBar = () => {
       top="0"
       left="0"
       right="0"
-      zIndex="999"
+      bg="white"
       display="flex"
       justifyContent="space-between"
       alignItems="center"
